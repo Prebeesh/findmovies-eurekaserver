8506d4c6b2107bacf99cf77e52abca5ff40a2886
Prebeesh.S <prebeesh1427@gmail.com>
2020-07-09 23:38:14 +0530

    Testing the changes in security config

--- src/main/java/com/prebeesh1427/eurekaserver/configuration/SecurityConfigiuration.java
+++ src/main/java/com/prebeesh1427/eurekaserver/configuration/SecurityConfigiuration.java
@@ -2,13 +2,17 @@ package com.prebeesh1427.eurekaserver.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@Order(1)
 public class SecurityConfigiuration extends WebSecurityConfigurerAdapter{
 
 	@Value("${EUREKA_USER_PASSWORD}")
@@ -20,7 +24,7 @@ public class SecurityConfigiuration extends WebSecurityConfigurerAdapter{
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password(encoder.encode(password))
-		.authorities("ADMIN");
+		.roles("USER");
 	}
 
 	@Override
