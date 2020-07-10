691d2a57a2a59d7003fbe5ce4faec2c2f0385ce4
Prebeesh.S <prebeesh1427@gmail.com>
2020-07-10 14:05:32 +0530

    Changed the security config with formlogin() so that a beautiful ui is shown for login

--- src/main/java/com/prebeesh1427/eurekaserver/configuration/SecurityConfigiuration.java
+++ src/main/java/com/prebeesh1427/eurekaserver/configuration/SecurityConfigiuration.java
@@ -35,6 +35,7 @@ public class SecurityConfigiuration extends WebSecurityConfigurerAdapter{
 		.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
+		.formLogin().and()
 		.httpBasic();
 	}
 
